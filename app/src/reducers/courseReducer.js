696ff1d3a271d6e5924c01455ebdf9439663a33c
Mikael Mrozik <mikael.mrozik@deviate.net.nz>
2017-09-26 11:59:10 +1300

    Async in Redux

--- app/src/reducers/courseReducer.js
+++ app/src/reducers/courseReducer.js
@@ -1,12 +1,22 @@
 import * as types from '../actions/actionTypes';
 
-export default function createReducer(state = [], action) {
+export default function courseReducer(state = [], action) {
   switch (action.type) {
-    case types.CREATE_COURSE:
-      return [...state,
-        Object.assign({}, action.course)
-      ];
+    case types.LOAD_COURSES_SUCCESS:
+      return action.courses;
     default:
       return state;
   }
 }
+
+
+// export default function createReducer(state = [], action) {
+//   switch (action.type) {
+//     case types.CREATE_COURSE:
+//       return [...state,
+//         Object.assign({}, action.course)
+//       ];
+//     default:
+//       return state;
+//   }
+// }
