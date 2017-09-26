ecac64d09499dc60061d84bac977a5f1a6c83d1a
Mikael Mrozik <mikael.mrozik@deviate.net.nz>
2017-09-27 11:07:51 +1300

    Async writes in Redux

--- app/src/api/mockAuthorApi.js
+++ app/src/api/mockAuthorApi.js
@@ -77,3 +77,5 @@ class AuthorApi {
     });
   }
 }
+
+export default AuthorApi;
--- app/src/components/course/CoursesPage.js
+++ app/src/components/course/CoursesPage.js
@@ -3,14 +3,20 @@ import {connect} from 'react-redux';
 import {bindActionCreators} from 'redux';
 import * as courseActions from '../../actions/courseActions';
 import CourseList from './CourseList';
+import {browserHistory} from 'react-router';
 
 class CoursesPage extends React.Component {
   constructor (props, context) {
       super (props, context);
+      this.redirectToAddCoursePage = this.redirectToAddCoursePage.bind(this);
   }
 
   courseRow(course, index){
     return <div key={index}>{course.title}</div>;
+  }
+
+  redirectToAddCoursePage(){
+    browserHistory.push('/course');
   }
 
 
@@ -19,6 +25,11 @@ class CoursesPage extends React.Component {
     return(
       <div>
         <h1>Courses</h1>
+          <input
+             type="submit"
+             value="Add course"
+             onClick={this.redirectToAddCoursePage}
+             className="btn btn-primary" />
         <CourseList courses={courses} />
       </div>
      );
